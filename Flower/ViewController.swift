116181186a5facc6f4bfdcd916455f152ede4f83
Eldon <eldonjiang888@gmail.com>
2019-03-09 17:42:05 -0500

    Initial Commit

---
+++ Flower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flower
+//
+//  Created by Eldon Jiang on 2019-03-09.
+//  Copyright © 2019 Eldon Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
